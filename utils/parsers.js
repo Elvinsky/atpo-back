cb81932b9b508d18e3c5a01c4a73fe7cbbd9f01a
Artem Mironchik <tawerka.power@gmail.com>
2023-12-09 03:15:23 +0300

    feat: implement multiple variables on the same line, variables without initializing, rewrite variable expression parsing logic, add validation

--- utils/parsers.js
+++ utils/parsers.js
@@ -1,7 +1,9 @@
+import calculator from "../calculator.js";
+
 const possibleDeclarations = ["let", "const"];
 
 export function replaceVariables(code) {
-  const variables = {};
+  let variables = {};
 
   let lines = code.split("\n");
 
@@ -9,34 +11,39 @@ export function replaceVariables(code) {
     line = line.trim();
 
     if (possibleDeclarations.some((declaration) => line.startsWith(declaration))) {
-      const [keyword, name, sign, value] = line.split(" ");
+      const [declaration, expression] = line.split(/(?<=^\S+)\s/);
 
-      if (!possibleDeclarations.includes(keyword)) {
-        throw new SyntaxError(`Unexpected variable declaration '${keyword}'.`);
+      if (!expression) {
+        throw new ReferenceError(`${declaration} is not defined.`);
       }
 
-      if (sign !== '=') {
-        throw new SyntaxError(`Unexpected token '${sign}'.`);
-      }
+      const splittedExpression = expression.split(',');
 
-      if (variables.hasOwnProperty(name)) {
-        throw new Error(`Variable ${name} is already defined.`);
-      } else {
-        variables[name] = { keyword, value };
+      for (let i = 0; i < splittedExpression.length; i++) {
+        const expressionVariables = parseVariableExpression(splittedExpression[i].trim(), declaration, variables);
+
+        variables = {
+          ...variables,
+          ...expressionVariables,
+        }
       }
     } else if (line.indexOf('=') > -1) {
       const [name, sign, value] = line.split(" ");
+      
+      if (!possibleDeclarations.includes(name) && !variables.hasOwnProperty(name)) {
+        throw new SyntaxError(`Unexpected variable declaration '${name}'.`);
+      }
+      
+      if (!variables.hasOwnProperty(name)) {
+        throw new ReferenceError(`Variable ${name} is not defined.`);
+      }
 
       if (sign !== '=') {
         throw new SyntaxError(`Unexpected token '${sign}'.`);
       }
 
-      if (!variables.hasOwnProperty(name)) {
-        throw new Error(`Variable ${name} is not defined.`);
-      }
-
-      if (variables[name].keyword === "const") {
-        throw new Error(`Cannot reassign const variable ${name}.`);
+      if (variables[name].declaration === "const") {
+        throw new TypeError(`Assignment to constant variable ${name}.`);
       } else {
         variables[name].value = value;
       }
@@ -52,4 +59,52 @@ export function replaceVariables(code) {
       return line;
     }
   }
+}
+
+const parseVariableExpression = (expression, declaration, variables) => {
+  const expressionVariables = {};
+
+  let [variable, value] = expression.trim().split(/(?<!=)=(?!=)/g);
+
+  variable = variable.trim();
+
+  if (value) {
+    const expressionRegex = new RegExp(/^[a-zA-Z_$][a-zA-Z0-9_$]*\s*=\s*[^=]+$/g);
+  
+    if (!expressionRegex.test(expression)) {
+      throw new SyntaxError(`Invalid variable assignment syntax: ${expression}`);
+    }
+  }
+
+  const variableRegex = new RegExp(/^([a-zA-Z_$][a-zA-Z\d_$]*)$/g);
+
+  if (value && !variableRegex.test(variable)) {
+    throw new SyntaxError(`Invalid variable name: ${variable}.`);
+  }
+
+  if (declaration === 'const') {
+    if (!value) {
+      throw new SyntaxError('Missing initializer in const declaration');
+    }
+
+    if (variables.hasOwnProperty(variable)) {
+      throw new TypeError(`Assignment to constant variable ${variable}.`);
+    }
+
+    expressionVariables[variable] = { declaration, value: calculator(value.trim()) };
+  } else {
+    if (!value) {
+      expressionVariables[variable] = { declaration, value: undefined };
+
+      return expressionVariables;
+    }
+
+    if (variables.hasOwnProperty(variable)) {
+      throw new SyntaxError(`Identifier '${variable}' has already been declared.`);
+    }
+
+    expressionVariables[variable] = { declaration, value: calculator(value.trim()) };
+  }
+
+  return expressionVariables;
 }
